3d6f2217707363425d44a29acd5e3d50bd1ea336
Inf3rnus187 <inf3rnus187@gmail.com>
2025-03-27 14:19:00 +0100

    Update install-pcoip.ps1

--- install-pcoip.ps1
+++ install-pcoip.ps1
@@ -1,3 +1,26 @@
+function Import-GitHubRegFile {
+$URL = 
+$TempFilePath = "$env:TEMP\temp_import.reg"
+
+    try {
+        Write-Host "Téléchargement du fichier .reg depuis GitHub..." -ForegroundColor Cyan
+        Invoke-WebRequest -Uri $GitHubRawUrl -OutFile $TempFilePath -ErrorAction Stop
+
+        Write-Host "Importation du fichier dans le registre..." -ForegroundColor Cyan
+        reg import $TempFilePath
+
+        Write-Host "✅ Fichier importé avec succès depuis : $GitHubRawUrl" -ForegroundColor Green
+    } catch {
+        Write-Error "❌ Une erreur est survenue : $_"
+    } finally {
+        if (Test-Path $TempFilePath) {
+            Remove-Item $TempFilePath -Force
+            Write-Host "🧹 Fichier temporaire supprimé." -ForegroundColor Yellow
+        }
+    }
+}
+
+
 Write-Host "Installing PCoIP Graphic Agent" -ForegroundColor Green
 $process = Start-Process -FilePath "$env:TEMP\pcoip.exe" -ArgumentList "/S /NoPostReboot _?$env:TEMP\pcoip.exe" -Wait -PassThru; $process.ExitCode
 Write-Host "Done" -ForegroundColor Green
