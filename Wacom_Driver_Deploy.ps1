50e2930775a7f8ea3168b11cad10485cebd1f5be
Inf3rnus187 <inf3rnus187@gmail.com>
2025-03-28 10:19:01 +0100

    Update Wacom_Driver_Deploy.ps1

--- Wacom_Driver_Deploy.ps1
+++ Wacom_Driver_Deploy.ps1
@@ -5,9 +5,9 @@ Unblock-File -Path "$env:TEMP\psadt.zip"
 
 Expand-Archive -Path "$env:TEMP\psadt.zip" -DestinationPath "$env:TEMP\PADT" -Force
 
-Copy-Item -Path "$env:TEMP\PADT\Toolkit\AppDeployToolkit" -Destination "$env:TEMP\WacomTablet\AppDeployToolkit" -Recurse
-Copy-Item -Path "$env:TEMP\PADT\Toolkit\Files" -Destination "$env:TEMP\WacomTablet\Files"
+Copy-Item -Path "$env:TEMP\PADT\Toolkit\AppDeployToolkit" -Destination "$env:TEMP\WacomTablet\AppDeployToolkit" -Recurse -Force
+Copy-Item -Path "$env:TEMP\PADT\Toolkit\Files" -Destination "$env:TEMP\WacomTablet\Files" -Force
 
 
-Invoke-WebRequest -Uri "https://github.com/Inf3rnus187/Script/raw/refs/heads/main/WacomTablet_6.4.9-2.exe" -OutFile "$env:TEMP\WacomTablet\Files\"
+Invoke-WebRequest -Uri "https://cdn.wacom.com/u/productsupport/drivers/win/professional/WacomTablet_6.4.9-2.exe" -OutFile "$env:TEMP\WacomTablet\Files\"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Inf3rnus187/Script/refs/heads/main/Wacom_Tablet_Driver_Install_and_Uninstall.ps1" -OutFile "$env:TEMP\WacomTablet\Files\Deploy-WacomTablet.ps1"
